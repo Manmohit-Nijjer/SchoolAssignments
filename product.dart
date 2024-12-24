3592498a1af427d2b8da854952093d2ceb4923d0
Manmohit-Nijjer <Nijjer2004@gmail.com>
2024-12-24 13:40:09 -0500

    Lab 4 - Mobile App Development

--- product.dart
+++ product.dart
@@ -1,6 +1,6 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file
+// This is merely changed files from the shrine template from flutter repositiory on git, this is to showcase the task assigned by my proffessor in our ability to edit and change files to include what we need. 
+
+
 import 'package:flutter/material.dart';
 
 class SubCategory {
